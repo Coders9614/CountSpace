d8c5f0cc38f4e9f6fc814fe0c38de00e65a948e0
Coders9614 <joonyong.kim1@outlook.com>
2024-06-06 09:12:03 -0600

    Add project files.

---
+++ CountSpace/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountSpace
+{
+    public class Program
+    {
+        public static int CountSpaces(string input)
+        {
+            int spaceCount = 0;
+            foreach (char c in input)
+            {
+                if (c == ' ')
+                {
+                    spaceCount++;
+                }
+            }
+            return spaceCount;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.Write("Please input a string: ");
+            string inputString = Console.ReadLine();
+
+            int spaces = CountSpaces(inputString);
+
+            Console.WriteLine($"\"{inputString}\" contains {spaces} spaces");
+            Console.ReadKey();
+        }
+    }
+}
